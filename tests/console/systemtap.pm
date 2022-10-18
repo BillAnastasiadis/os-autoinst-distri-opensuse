5f8787c139c6507cba44e884ab164f30c90f5a47
Bill Anastasiadis <vasilios.anastasiadis@suse.com>
2022-10-18 12:38:59 +0200

    Test 2

--- tests/console/systemtap.pm
+++ tests/console/systemtap.pm
@@ -18,7 +18,7 @@ use kdump_utils;
 use version_utils qw(is_sle);
 
 sub run {
-    my $self =  shift;
+    my $self =   shift;
     $self->select_serial_terminal;
     prepare_for_kdump();
     zypper_call("in systemtap systemtap-docs kernel-devel systemtap-server");
