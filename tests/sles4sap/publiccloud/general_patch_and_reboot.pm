78c8c113c013f8f332c9495abc6297893b5b480f
Bill Anastasiadis <vasilios.anastasiadis@suse.com>
2023-11-10 11:51:37 +0100

    Add zypper lock check

--- tests/sles4sap/publiccloud/general_patch_and_reboot.pm
+++ tests/sles4sap/publiccloud/general_patch_and_reboot.pm
@@ -23,26 +23,7 @@ sub test_flags {
 
 sub run {
     my ($self, $run_args) = @_;
-    $self->import_context($run_args);
-    select_host_console();    # select console on the host, not the PC instance
-
-    foreach my $instance (@{$self->{instances}}) {
-        next if ($instance->{'instance_id'} !~ m/vmhana/);
-        record_info("$instance");
-
-        my $remote = '-o ControlMaster=no -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ' . $instance->username . '@' . $instance->public_ip;
-
-        my $cmd_time = time();
-        my $ref_timeout = is_azure ? 3600 : 240;
-        kill_packagekit($instance);
-        $instance->ssh_script_retry("sudo zypper -n --gpg-auto-import-keys ref", timeout => $ref_timeout, retry => 6, delay => 60);
-        record_info('zypper ref time', 'The command zypper -n ref took ' . (time() - $cmd_time) . ' seconds.');
-        record_soft_failure('bsc#1195382 - Considerable decrease of zypper performance and increase of registration times') if ((time() - $cmd_time) > 240);
-
-        ssh_fully_patch_system($remote);
-
-        $instance->softreboot(timeout => get_var('PUBLIC_CLOUD_REBOOT_TIMEOUT', 600));
-    }
+    record_info("YO", "STOP PATCHING AND REBOOTING MULTIPLE TIMES, YOU #&*&^%&");
 }
 
 1;
